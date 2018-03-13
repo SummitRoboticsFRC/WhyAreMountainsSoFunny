6cdbd75938e08437e963c5a8467da5b0d1074831
splashface256 <splashface256@egmail.com>
2018-03-13 15:43:20 +0800

    =added initialization for pneumatics command

--- BecauseTheyAreHillAreas/src/org/usfirst/frc/team1014/robot/commands/PneumaticsCommand.java
+++ BecauseTheyAreHillAreas/src/org/usfirst/frc/team1014/robot/commands/PneumaticsCommand.java
@@ -17,11 +17,13 @@ public class PneumaticsCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    		Robot.pneumaticsSubsystem.solenoidValves();
+    		Robot.pneumaticsSubsystem.compressorStatus();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
+    		
     }
 
     // Make this return true when this Command no longer needs to run execute()
